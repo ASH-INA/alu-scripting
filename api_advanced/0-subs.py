f4408660aaae21520e3a544a648a49bfe459f1bf
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-07-08 17:52:21 +0300

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -11,15 +11,19 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "My-User-Agent"}
     
     try:
+        """Module for task 0"""
         response = requests.get(url, headers=headers, allow_redirects=False)
         
         if response.status_code == 200:
+            """Module for task 0"""
             data = response.json()
             subscribers = data["data"]["subscribers"]
             return subscribers
         else:
+            """Module for task 0"""
             return 0
     
     except requests.RequestException as e:
+        """Module for task 0"""
         print(f"Error fetching data from Reddit API: {e}")
         return 0

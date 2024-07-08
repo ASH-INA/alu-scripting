f6a0701b71e4d91bbd34db50c46a7715609915f1
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-07-08 17:48:33 +0300

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,11 +7,19 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "My-User-Agent"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data["data"]["subscribers"]
+            return subscribers
+        else:
+            return 0
+    
+    except requests.RequestException as e:
+        print(f"Error fetching data from Reddit API: {e}")
         return 0
-
-    return sub_info.json().get("data").get("subscribers")

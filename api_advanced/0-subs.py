46ba93b4495c7cd97bf6e2fd67744548183418e9
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-07-08 17:54:45 +0300

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+Importing requests module
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My-User-Agent"}
-    
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    all_data = response.json()
+
     try:
-        """Module for task 0"""
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            """Module for task 0"""
-            data = response.json()
-            subscribers = data["data"]["subscribers"]
-            return subscribers
-        else:
-            """Module for task 0"""
-            return 0
-    
+        return all_data.get('data').get('subscribers')
+
+    except:
+        return 0
     except requests.RequestException as e:
         """Module for task 0"""
         print(f"Error fetching data from Reddit API: {e}")

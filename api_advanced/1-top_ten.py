4652d033d1a6fe4e53321b1852a00e560c0f10fc
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-07-20 10:22:21 +0300

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,23 +5,12 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-
     response = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if response.status_code == 200:
-        """Extract the JSON data from the response"""
-        data = response.json()
-        
-        """Extract and print titles of the first 10 hot posts"""
-        print(f"Top 10 hot posts in r/{subreddit}:")
-        for post in data['data']['children']:
-            print(post['data']['title'])
-    else:
-        """If request was not successful, print None"""
-        print(f"Error: Unable to fetch posts from r/{subreddit}. Status code: {response.status_code}")
+    if response.status_code == 404:
         print("None")
-
-if __name__ == "__main__":
-    subreddit = input("Enter the subreddit name: ")
-    top_ten(subreddit)
+        return
+    data = response.json()
+    for post in data['data']['children']:
+            print(post['data']['title'])

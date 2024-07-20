42a7b0bc53125a11f214ebf531db6ec7cb29ad2e
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-07-20 11:05:47 +0300

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Mozilla/5.0"
     }
     params = {
         "limit": 10
@@ -16,10 +16,9 @@ def top_ten(subreddit):
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
-        return
     data = response.json()
     for post in data['data']['children']:
         print(post['data']['title'])
 
 if __name__ == "__main__":
-    top_ten('python')
+    top_ten('Python')

d574f1a8a4565dadf0ac6d3c534dca71781cd175
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-07-20 10:40:27 +0300

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,8 +5,14 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
-                            headers={"User-Agent": "My-User-Agent"},
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")

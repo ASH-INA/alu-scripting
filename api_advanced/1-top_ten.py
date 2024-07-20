843d147d12f6d382cff2f96702adc7b7c77d44d1
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-07-20 11:31:11 +0300

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""This script queries the Reddit API to fetch."""
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    """Fetches and prints the titles of the first 10 hot posts for given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                      '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
-        "limit":"10"
+        "limit": 10
     }
-    
-    response = requests.get(url, headers=headers, params=params)
-     if response.status_code == 200:
-        data = response.json()
-        
-        for i, post in enumerate(data['data']['children'][:10], start=1):
-            print(f"{i}. {post['data']['title']}")
-    else:
-        print(None)
-
-if __name__ == "__main__":
-    top_ten(subreddit)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]

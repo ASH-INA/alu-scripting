87e6ebcf4cec198032141ffc5aa378f215475bb0
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-07-20 11:25:27 +0300

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-
-"""
-This script queries the Reddit API to fetch."""
+"""This script queries the Reddit API to fetch."""
 
 import sys
 import requests
-import logging
 
 def top_ten(subreddit):
-    """
-    Fetches and prints the titles of the first 10 hot posts for given subreddit."""
-    # Reddit API endpoint URL for getting hot posts in a subreddit
+    """Fetches and prints the titles of the first 10 hot posts for given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     
-    # Set custom User-Agent header to prevent 429 (rate limit exceeded) responses
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                       '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
@@ -23,14 +17,8 @@ def top_ten(subreddit):
     }
     
     response = requests.get(url, headers=headers, params=params)
+     data = response.json()
     
-    # Raise an exception for bad status codes
-    response.raise_for_status()
-    
-    # Extract JSON data from the response
-    data = response.json()
-    
-    # Iterate over the first 10 posts and print their titles
     for i, post in enumerate(data['data']['children'][:10], start=1):
         print(f"{i}. {post['data']['title']}")
 
